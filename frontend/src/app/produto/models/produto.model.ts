34ade99bfdd952116f0b192c93544c4daea7c254
Ivan Consalter <ivanconsalter1@gmail.com>
2022-11-04 17:30:57 -0300

    added produto model

---
+++ frontend/src/app/produto/models/produto.model.ts
@@ -0,0 +1,17 @@
+export class Produto {
+  id: string;
+  fornecedorId: string;
+  nome: string;
+  descricao: string;
+  imagemUpload: string;
+  imagem: string;
+  valor: number;
+  dataCadastro: string;
+  ativo: boolean;
+  nomeFornecedor: string;
+}
+
+export interface IFornecedor{
+  id: string;
+  nome: string;
+}
